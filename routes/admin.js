8632285bc5c7b89faa236bfac8093dcb3ea23261
Ramaneswaran <ramannewdelhi44@gmail.com>
2019-09-07 20:40:36 +0530

    Added routes for admin routes

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,11 @@ router.get('/', (req, res)=>{
     res.render('admin');
 });
 
+router.get('/addcourse', (req, res)=>{
+    res.render('addcourse');
+});
+
+router.get('/addpaper', (req, res)=>{
+    res.render('addpaper');
+});
 module.exports = router;
