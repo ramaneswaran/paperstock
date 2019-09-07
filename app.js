526cfd41f6efbdfbc615a7dfe0d745797942a33a
Ramaneswaran <ramannewdelhi44@gmail.com>
2019-09-07 20:03:05 +0530

    Added .env configuraion and connected to database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes/api');
 const mongoose = require('mongoose');
+require('dotenv/config');
 
 //Set up express app
 const app = express();
@@ -14,6 +15,10 @@ app.use(express.static('public'));
 //Set up template engine
 app.set('view engine', 'ejs');
 
+//CONNECT TO DB
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, ()=>{
+    console.log('Connected to DB');
+});
 
 app.listen(process.env.port || 3000, ()=>{
     console.log("Listening to port")
